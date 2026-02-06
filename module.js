5c6a1dbb47aa0a0bf77295fd2b15d072d23623d1
youen <youen.valun@gmail.com>
2026-02-06 19:07:39 +0100

    finish atctivity 1

--- module.js
+++ module.js
@@ -5,7 +5,11 @@
  * @returns {number} - L'âge de la personne en années.
  */
 
-function calculateAge(p) {
+export function calculateAge(p) {
+  if (!p) {
+    throw new Error("Missing parameter p");
+  }
+
   let dateDiff = new Date(Date.now() - p.birth.getTime());
   let age = Math.abs(dateDiff.getUTCFullYear() - 1970);
   return age;
