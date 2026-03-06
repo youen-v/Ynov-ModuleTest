520a43d003e2913f54df4ca1f00c7729480e74e4
youen <youen.valun@gmail.com>
2026-03-06 17:12:22 +0100

    fix sql

--- my-app/sql/migration-v001.sql
+++ my-app/sql/migration-v001.sql
@@ -1,7 +1,8 @@
-CREATE DATABASE ynov_ci;
-CREATE TABLE ynov_ci.utilisateur (
+CREATE DATABASE IF NOT EXISTS ynov_ci;
+USE ynov_ci;
+CREATE TABLE IF NOT EXISTS utilisateur (
     id INT AUTO_INCREMENT PRIMARY KEY,
     nom VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL
 );
-INSERT INTO ynov_ci.utilisateur (nom, email) VALUES ('Alice', 'alice@example.com');
+INSERT INTO utilisateur (nom, email) VALUES ('Alice', 'alice@example.com');
