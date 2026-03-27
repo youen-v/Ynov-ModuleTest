807d8ae7aaa36800082ac08a5415e402386d6e67
youen <youen.valun@gmail.com>
2026-03-27 19:10:14 +0100

    fix db

--- my-app/sql/migration-v001.sql
+++ my-app/sql/migration-v001.sql
@@ -3,6 +3,9 @@ USE ynov_ci;
 CREATE TABLE IF NOT EXISTS utilisateur (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL
+    email VARCHAR(255) NOT NULL,
+    birthDate DATE NOT NULL,
+    zip VARCHAR(10) NOT NULL,
+    city VARCHAR(255) NOT NULL
 );
 INSERT INTO utilisateur (name, email) VALUES ('Alice', 'alice@example.com');
