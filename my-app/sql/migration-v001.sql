cfd6a064e471d6eebba55b21fb710968070b71aa
youen <youen.valun@gmail.com>
2026-03-27 20:05:33 +0100

    fix db

--- my-app/sql/migration-v001.sql
+++ my-app/sql/migration-v001.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS utilisateur (
     id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(255) NOT NULL,
     email VARCHAR(255) NOT NULL,
-    birthDate DATE NOT NULL,
+    birthDate VARCHAR(10) NOT NULL,
     zip VARCHAR(10) NOT NULL,
     city VARCHAR(255) NOT NULL
 );
